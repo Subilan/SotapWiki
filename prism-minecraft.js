b55d6844474498dbd5f092b6816b4ed1a1432c0d
Subilan <suyuanliang1995@gmail.com>
2020-07-27 23:48:03 +0800

    Fix label after returning new line

--- prism-minecraft.js
+++ prism-minecraft.js
@@ -6,7 +6,7 @@
         'optional': /\[\S+\]/,
         'number': /[\+-]?\d+/,
         'colorcode': /&(a|b|c|d|e|f|k|l|o|r|1|2|3|4|5|6|7|8|9|0)/,
-        'label': /^\/\w+/,
+        'label': /^\n?\/?\w+/,
         'argument': /\S+/,
     }
 }(Prism));
