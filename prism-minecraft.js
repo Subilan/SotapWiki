ffbeba659dfc6fad2b692f13dfc08700b270672f
WindSpirit <simon343riley@gmail.com>
2020-08-04 17:04:21 +0800

    fix regular

--- prism-minecraft.js
+++ prism-minecraft.js
@@ -1,12 +1,12 @@
 (function (Prism) {
     Prism.languages.minecraft = {
         'condition': /\(.+\)/,
-        'choisy': /<\w+(\|(\w|\.)+)+>/,
-        'required': /<\S+>/,
+        'choisy': /<(\S|\ )+(\|(\S|\ )+)+>/,
+        'required': /<((\S|\ )(?!\|))+>/,
         'optional': /\[\S+\]/,
         'number': /[\+-]?\d+/,
         'colorcode': /&(a|b|c|d|e|f|k|l|m|n|o|r|1|2|3|4|5|6|7|8|9|0)/,
-        'label': /^\n?\/?\w+/,
+        'label': /\/\S+\s?/,
         'argument': /\S+/,
     }
 }(Prism));
