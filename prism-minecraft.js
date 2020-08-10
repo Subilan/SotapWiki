7f8d1faa11cf3f91fef60ba2787e90a44bdeb50b
WindSpirit <simon343riley@gmail.com>
2020-08-10 18:01:27 +0800

    Regex flags add global and multi line

--- prism-minecraft.js
+++ prism-minecraft.js
@@ -1,12 +1,12 @@
 (function (Prism) {
     Prism.languages.minecraft = {
-        'condition': /\(.+\)/,
-        'choisy': /<(\S|\ )+(\|(\S|\ )+)+>/,
-        'required': /<((\S|\ )(?!\|))+>/,
-        'optional': /\[\S+\]/,
-        'number': /[\+-]?\d+/,
-        'colorcode': /&(a|b|c|d|e|f|k|l|m|n|o|r|1|2|3|4|5|6|7|8|9|0)/,
-        'label': /\/\S+\s?/,
-        'argument': /\S+/,
+        'condition': /\(.+\)/gm,
+        'choisy': /<(\S|\ )+(\|(\S|\ )+)+>/gm,
+        'required': /<((\S|\ )(?!\|))+>/gm,
+        'optional': /\[\S+\]/gm,
+        'number': /[\+-]?\d+/gm,
+        'colorcode': /&(a|b|c|d|e|f|k|l|m|n|o|r|1|2|3|4|5|6|7|8|9|0)/gm,
+        'label': /\/\S+\s?/gm,
+        'argument': /\S+/gm,
     }
 }(Prism));
